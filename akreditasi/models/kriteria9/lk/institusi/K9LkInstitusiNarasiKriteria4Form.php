3389b412f9e14e22316b5e9f2a42210c6da604f8
Adryan Eka Vandra <adryanekavandra@gmail.com>
2019-10-22 18:26:39 +0700

    create model narasi lk

---
+++ akreditasi/models/kriteria9/lk/institusi/K9LkInstitusiNarasiKriteria4Form.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Project: kriteria.
+ * @author Adryan Eka Vandra <adryanekavandra@gmail.com>
+ *
+ * Date: 10/22/2019
+ * Time: 5:29 PM
+ */
+
+namespace akreditasi\models\kriteria9\lk\prodi;
+
+
+
+use common\models\kriteria9\lk\institusi\K9LkInstitusiKriteria4;
+
+class K9LkInstitusiNarasiKriteria4Form extends K9LkInstitusiKriteria4
+{
+
+    public function beforeSave($insert)
+    {
+        $this->updateProgress();
+        $this->lkInstitusi->updateProgress();
+        $this->lkInstitusi->akreditasiInstitusi->updateProgress();
+        return parent::beforeSave($insert);
+    }
+
+    public function updateProgress()
+    {
+        $count = 0;
+
+        $exclude = ['id', 'id_lk_institusi', 'progress', 'created_at', 'updated_at'];
+
+        $filters = array_filter($this->attributes, function ($attribute) use ($exclude) {
+            return in_array($attribute, $exclude) === false;
+        }, ARRAY_FILTER_USE_KEY);
+
+        $total = sizeof($filters);
+
+        foreach ($filters as $attribute) {
+            if ($attribute !== null) {
+                $count += 1;
+            }
+        }
+
+        $progress = round(($count / $total) * 100, 2);
+
+        $this->progress = $progress;
+
+        return true;
+    }
+}
---
+++ akreditasi/models/kriteria9/lk/prodi/K9LkProdiNarasiKriteria5Form.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Project: kriteria.
+ * @author Adryan Eka Vandra <adryanekavandra@gmail.com>
+ *
+ * Date: 10/22/2019
+ * Time: 5:29 PM
+ */
+
+namespace akreditasi\models\kriteria9\lk\prodi;
+
+
+use common\models\kriteria9\lk\prodi\K9LkProdiKriteria5;
+
+class K9LkProdiNarasiKriteria5Form extends K9LkProdiKriteria5
+{
+
+    public function beforeSave($insert)
+    {
+        $this->updateProgress();
+        $this->lkProdi->updateProgress();
+        $this->lkProdi->akreditasiProdi->updateProgress();
+        return parent::beforeSave($insert);
+    }
+
+    public function updateProgress()
+    {
+        $count = 0;
+
+        $exclude = ['id', 'id_lk_prodi', 'progress', 'created_at', 'updated_at'];
+
+        $filters = array_filter($this->attributes, function ($attribute) use ($exclude) {
+            return in_array($attribute, $exclude) === false;
+        }, ARRAY_FILTER_USE_KEY);
+
+        $total = sizeof($filters);
+
+        foreach ($filters as $attribute) {
+            if ($attribute !== null) {
+                $count += 1;
+            }
+        }
+
+        $progress = round(($count / $total) * 100, 2);
+
+        $this->progress = $progress;
+
+        return true;
+    }
+}
