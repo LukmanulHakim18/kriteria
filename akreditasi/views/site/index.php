8d58ff0912d9573460ee34f5b7f47b8e1ec1fe0d
Novrii <38958308+Novrii@users.noreply.github.com>
2020-08-27 15:36:09 +0700

    update perhitungan led

--- akreditasi/views/site/index.php
+++ akreditasi/views/site/index.php
@@ -12,8 +12,20 @@ $this->title = 'Beranda';
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
-<div>
-    <p>Home</p>
+<div class="kt-portlet">
+    <div class="kt-space-30"></div>
+    <h1 class="text-center"><?= Yii::$app->params['institusi'] ?></h1>
+
+    <div class="text-center" style="margin-top: 30px">
+        <img src="<?= Yii::getAlias('@web/upload/struktur/struktur.png') ?>">
+    </div>
+    <div class="text-center" style="margin-top: 10px">
+        <img src="<?= Yii::getAlias('@web/upload/struktur/struktur2.png') ?>">
+    </div>
+    <div class="text-center" style="margin-top: 20px">
+        <img src="<?= Yii::getAlias('@web/upload/struktur/struktur3.png') ?>">
+    </div>
+
 </div>
 
 
