c80393a2374e9c11f73b8e9ef73f40ff0cc30fa7
Adryan Eka Vandra <adryanekavandra@gmail.com>
2020-08-29 23:26:51 +0700

    fixing profil user role migration

--- console/migrations/m200507_080417_create_profil_user_role_table.php
+++ console/migrations/m200507_080417_create_profil_user_role_table.php
@@ -21,7 +21,7 @@ class m200507_080417_create_profil_user_role_table extends Migration
             'id' => $this->primaryKey(),
             'id_profil'=>$this->integer(),
             'external_id'=>$this->integer(),
-            'type'=>$this->string(10)
+            'type'=>$this->string(15)
         ],$tableOptions);
 
         $this->addForeignKey('fk-profil_user_role-user','{{%profil_user_role}}','id_profil','{{%profil_user}}','id','cascade','cascade');
