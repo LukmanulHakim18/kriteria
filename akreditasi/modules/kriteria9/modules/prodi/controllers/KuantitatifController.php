b27a119caca5b5a4bb711ac90f3dfe300a031c84
Adryan Eka Vandra <adryanekavandra@gmail.com>
2020-12-11 11:33:45 +0700

    pre-testing export excel
    Signed-off-by: Adryan Eka Vandra <adryanekavandra@gmail.com>

--- akreditasi/modules/kriteria9/modules/prodi/controllers/KuantitatifController.php
+++ akreditasi/modules/kriteria9/modules/prodi/controllers/KuantitatifController.php
@@ -5,7 +5,6 @@ namespace akreditasi\modules\kriteria9\modules\prodi\controllers;
 
 
 use akreditasi\modules\kriteria9\controllers\BaseController;
-use Carbon\Carbon;
 use common\helpers\kriteria9\K9ProdiDirectoryHelper;
 use common\models\kriteria9\akreditasi\K9Akreditasi;
 use common\models\kriteria9\akreditasi\K9AkreditasiProdi;
@@ -13,11 +12,9 @@ use common\models\kriteria9\forms\kuantitatif\K9PencarianKuantitatifForm;
 use common\models\kriteria9\kuantitatif\prodi\K9DataKuantitatifProdi;
 use common\models\ProgramStudi;
 use Yii;
-use yii\base\Exception;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Url;
 use yii\web\BadRequestHttpException;
-use yii\web\UploadedFile;
 
 class KuantitatifController extends BaseController
 {
@@ -56,41 +53,40 @@ class KuantitatifController extends BaseController
         $id_akre = K9AkreditasiProdi::find()->where(['id_prodi' => $prodi])->one();
 
         $dataKuantitatifProdi = K9DataKuantitatifProdi::find()->where(['id_akreditasi_prodi' => $id_akre->id,])->all();
-
-        $model = new K9DataKuantitatifProdi();
-
-        if ($model->load(Yii::$app->request->post())) {
-
-//            var_dump($model->akreditasiProdi);
-//            exit();
-
-            $carbon = Carbon::now('Asia/Jakarta');
-            $tgl = $carbon->format('U');
-
-            $file = UploadedFile::getInstance($model, 'nama_dokumen');
-            $fileName = $tgl . '-' . $file->getBaseName() . '.' . $file->getExtension();
-            $model->id_akreditasi_prodi = $id_akre->id;
-            $model->nama_dokumen = $fileName;
-            $path = K9ProdiDirectoryHelper::getKuantitatifPath($model->akreditasiProdi);
-
-            if (!$file->saveAs("$path/$fileName")) {
-                throw new Exception("Gagal Mengupload File");
-            }
-
-            if (!$model->save()) {
-                throw new Exception("Gagal Menyimpan Data Kuantitatif");
-            }
-
-            Yii::$app->session->setFlash('success', 'Berhasil Mengupload Dokumen Kuantitatif.');
-
-            $this->redirect(Url::current());
-
-        }
+//        $model = new K9DataKuantitatifProdi();
+//
+//        if ($model->load(Yii::$app->request->post())) {
+//
+////            var_dump($model->akreditasiProdi);
+////            exit();
+//
+//            $carbon = Carbon::now('Asia/Jakarta');
+//            $tgl = $carbon->format('U');
+//
+//            $file = UploadedFile::getInstance($model, 'nama_dokumen');
+//            $fileName = $tgl . '-' . $file->getBaseName() . '.' . $file->getExtension();
+//            $model->id_akreditasi_prodi = $id_akre->id;
+//            $model->nama_dokumen = $fileName;
+//            $path = K9ProdiDirectoryHelper::getKuantitatifPath($model->akreditasiProdi);
+//
+//            if (!$file->saveAs("$path/$fileName")) {
+//                throw new Exception("Gagal Mengupload File");
+//            }
+//
+//            if (!$model->save()) {
+//                throw new Exception("Gagal Menyimpan Data Kuantitatif");
+//            }
+//
+//            Yii::$app->session->setFlash('success', 'Berhasil Mengupload Dokumen Kuantitatif.');
+//
+//            $this->redirect(Url::current());
+//
+//        }
 
         return $this->render('isi', [
             'akreprodis1' => $id_akre,
             'dataKuantitatifProdi' => $dataKuantitatifProdi,
-            'model' => $model
+//            'model' => $model
         ]);
     }
 
--- common/jobs/KuantitatifProdiExportJob.php
+++ common/jobs/KuantitatifProdiExportJob.php
@@ -4,8 +4,16 @@
 namespace common\jobs;
 
 
+use Carbon\Carbon;
+use common\helpers\kriteria9\K9ProdiDirectoryHelper;
+use common\models\kriteria9\kuantitatif\prodi\K9DataKuantitatifProdi;
 use common\models\kriteria9\lk\prodi\K9LkProdi;
+use common\models\ProgramStudi;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use Symfony\Component\DomCrawler\Crawler;
 use yii\base\BaseObject;
+use yii\helpers\Inflector;
 use yii\queue\JobInterface;
 
 class KuantitatifProdiExportJob extends BaseObject implements JobInterface
@@ -16,10 +24,165 @@ class KuantitatifProdiExportJob extends BaseObject implements JobInterface
     public $lk;
     public $template;
 
+    /**
+     * @var Spreadsheet
+     */
+    private $spreadsheet;
 
     public function execute($queue)
     {
 
+        $reader = IOFactory::createReader('Xlsx');
+        $this->spreadsheet = $reader->load($this->template);
+        $this->export($queue);
+
+    }
+
+    private function export($queue)
+    {
+        $akreditasiProdi = $this->lk->akreditasiProdi;
+        $prodi = $akreditasiProdi->prodi;
+
+        switch ($prodi->jenjang) {
+            case ProgramStudi::JENJANG_DIPLOMA:
+                break;
+            case ProgramStudi::JENJANG_SARJANA:
+                $this->tabel1($prodi->jenjang);
+                break;
+            case ProgramStudi::JENJANG_SARJANA_TERAPAN:
+                break;
+            case ProgramStudi::JENJANG_MAGISTER:
+                break;
+            case ProgramStudi::JENJANG_MAGISTER_TERAPAN:
+                break;
+            case ProgramStudi::JENJANG_DOKTOR:
+                break;
+            case ProgramStudi::JENJANG_DOKTOR_TERAPAN:
+                break;
+        }
+
+        $writer = IOFactory::createWriter($this->spreadsheet, 'Xlsx');
+
+        $timestamp = Carbon::now()->timestamp;
+        $filename = "$timestamp-matriks-kuantitatif-" . Inflector::slug($prodi->nama) . '.xlsx';
+
+
+        $path = K9ProdiDirectoryHelper::getKuantitatifPath($akreditasiProdi);
+        $writer->save("$path/$filename");
+
+        $model = K9DataKuantitatifProdi::findOne(['id_akreditasi_prodi' => $akreditasiProdi->id]);
+        if (!$model) {
+            $model = new K9DataKuantitatifProdi();
+            $model->id_akreditasi_prodi = $akreditasiProdi->id;
+        }
+        $model->nama_dokumen = 'Matriks Kuantitatif ' . $prodi->nama . '(' . $akreditasiProdi->akreditasi->tahun . ')';
+        $model->isi_dokumen = $filename;
+        $model->save(false);
+    }
+
+    private function tabel1($jenjang)
+    {
+        //get Kriteria 1 LK
+        $tabel = $this->lk->k9LkProdiKriteria1s->k9LkProdiKriteria1Narasi;
+
+        //1-1
+        $crawler = new Crawler($tabel->_1__1);
+        $data = $crawler->filter('tbody')->filter('tr')->each(function ($tr, $i) {
+            return $tr->filter('td')->each(function ($td, $i) {
+                return trim($td->text());
+            });
+        });
+        array_pop($data);
+
+        $contentStartRow = 12;
+        $currentContentRow = 12;
+        $currentWorksheet = 3;
+
+        foreach ($data as $item) {
+            $this->spreadsheet->getSheet($currentWorksheet)->insertNewColumnBefore($currentContentRow + 1, 1);
+            $this->spreadsheet->getSheet($currentWorksheet)
+                ->setCellValue('A' . $currentContentRow, $item[0])
+                ->setCellValue('B' . $currentContentRow, $item[1])
+                ->setCellValue('C' . $currentContentRow, $item[2])
+                ->setCellValue('D' . $currentContentRow, $item[3])
+                ->setCellValue('E' . $currentContentRow, $item[4])
+                ->setCellValue('F' . $currentContentRow, $item[5])
+                ->setCellValue('G' . $currentContentRow, $item[6])
+                ->setCellValue('H' . $currentContentRow, $item[7])
+                ->setCellValue('I' . $currentContentRow, $item[8])
+                ->setCellValue('J' . $currentContentRow, $item[9]);
+
+            $currentContentRow++;
+
+        }
+
+        $this->spreadsheet->getSheet($currentWorksheet)->removeRow($currentContentRow, 1);
+
+        //1-2
+        $crawler = new Crawler($tabel->_1__2);
+        $data = $crawler->filter('tbody')->filter('tr')->each(function ($tr, $i) {
+            return $tr->filter('td')->each(function ($td, $i) {
+                return trim($td->text());
+            });
+        });
+        array_pop($data);
+
+        $contentStartRow = 12;
+        $currentContentRow = 12;
+        $currentWorksheet = 4;
+
+        foreach ($data as $item) {
+            $this->spreadsheet->getSheet($currentWorksheet)->insertNewColumnBefore($currentContentRow + 1, 1);
+            $this->spreadsheet->getSheet($currentWorksheet)
+                ->setCellValue('A' . $currentContentRow, $item[0])
+                ->setCellValue('B' . $currentContentRow, $item[1])
+                ->setCellValue('C' . $currentContentRow, $item[2])
+                ->setCellValue('D' . $currentContentRow, $item[3])
+                ->setCellValue('E' . $currentContentRow, $item[4])
+                ->setCellValue('F' . $currentContentRow, $item[5])
+                ->setCellValue('G' . $currentContentRow, $item[6])
+                ->setCellValue('H' . $currentContentRow, $item[7])
+                ->setCellValue('I' . $currentContentRow, $item[8])
+                ->setCellValue('J' . $currentContentRow, $item[9]);
+
+            $currentContentRow++;
+
+        }
+
+        $this->spreadsheet->getSheet($currentWorksheet)->removeRow($currentContentRow, 1);
+
+        //1-3
+        $crawler = new Crawler($tabel->_1__1);
+        $data = $crawler->filter('tbody')->filter('tr')->each(function ($tr, $i) {
+            return $tr->filter('td')->each(function ($td, $i) {
+                return trim($td->text());
+            });
+        });
+        array_pop($data);
+
+        $contentStartRow = 12;
+        $currentContentRow = 12;
+        $currentWorksheet = 5;
+
+        foreach ($data as $item) {
+            $this->spreadsheet->getSheet($currentWorksheet)->insertNewColumnBefore($currentContentRow + 1, 1);
+            $this->spreadsheet->getSheet($currentWorksheet)
+                ->setCellValue('A' . $currentContentRow, $item[0])
+                ->setCellValue('B' . $currentContentRow, $item[1])
+                ->setCellValue('C' . $currentContentRow, $item[2])
+                ->setCellValue('D' . $currentContentRow, $item[3])
+                ->setCellValue('E' . $currentContentRow, $item[4])
+                ->setCellValue('F' . $currentContentRow, $item[5])
+                ->setCellValue('G' . $currentContentRow, $item[6])
+                ->setCellValue('H' . $currentContentRow, $item[7])
+                ->setCellValue('I' . $currentContentRow, $item[8])
+                ->setCellValue('J' . $currentContentRow, $item[9]);
+
+            $currentContentRow++;
+
+        }
+
+        $this->spreadsheet->getSheet(3)->removeRow($currentContentRow, 1);
     }
 
 }
