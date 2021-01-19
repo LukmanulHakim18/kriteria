0a09173c4163dd522dfdf30c78e432b282d5eaca
adryanev <adryanekavandra@gmail.com>
2021-01-19 15:12:02 +0700

    update main-local

--- environments/dev/console/config/main-local.php
+++ environments/dev/console/config/main-local.php
@@ -2,7 +2,10 @@
 return [
     'bootstrap' => ['gii'],
     'modules' => [
-        'gii' => 'yii\gii\Module',
-        'allowedIPs' => ['127.0.0.1', '::1', '192.168.2.2','192.168.10.1'], // adjust this to your needs
+        'gii' => [
+            'class' => 'yii\gii\Module',
+            'allowedIPs' => ['127.0.0.1', '::1', '192.168.2.2', '192.168.10.1'], // adjust this to your needs
+
+        ]
     ],
 ];
