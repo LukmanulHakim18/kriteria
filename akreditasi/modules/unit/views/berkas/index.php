137bebbb75b0893af6b917e330474af1b80a9ff6
adryanev <adryanekavandra@gmail.com>
2021-02-23 18:39:47 +0700

    bugfix berkas institusi

--- akreditasi/modules/unit/views/berkas/index.php
+++ akreditasi/modules/unit/views/berkas/index.php
@@ -1,8 +1,8 @@
 <?php
 
+use yii\grid\GridView;
 use yii\helpers\Html;
 use yii\helpers\Url;
-use yii\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -21,14 +21,18 @@ $this->params['breadcrumbs'][] = $this->title;
                         <i class="flaticon2-list-2"></i>
                     </span>
                     <h3 class="kt-portlet__head-title">
-                        <?= Html::encode($this->title) ?> <small>portlet sub title</small>
+                        <?= Html::encode($this->title) ?>
                     </h3>
                 </div>
                 <div class="kt-portlet__head-toolbar">
                     <div class="kt-portlet__head-wrapper">
                         <div class="kt-portlet__head-actions">
 
-                            <?= Html::button('<i class=flaticon2-add></i> Tambah Berkas', ['value' => Url::to(['create','unit'=>Yii::$app->request->get('unit')]), 'title' => 'Tambah Berkas', 'class' => 'showModalButton btn btn-success btn-elevate btn-elevate-air']); ?>
+                            <?= Html::button('<i class=flaticon2-add></i> Tambah Berkas', [
+                                'value' => Url::to(['create', 'unit' => Yii::$app->request->get('unit')]),
+                                'title' => 'Tambah Berkas',
+                                'class' => 'showModalButton btn btn-success btn-elevate btn-elevate-air'
+                            ]); ?>
                         </div>
                     </div>
                 </div>
@@ -37,41 +41,49 @@ $this->params['breadcrumbs'][] = $this->title;
                 <div class="berkas-index">
 
 
+                    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-
-                                                                    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-                                            <?= GridView::widget([
-                                            'dataProvider' => $dataProvider,
-                                            'columns' => [
-                                            ['class' => 'yii\grid\SerialColumn','header'=>'No'],
+                    <?= GridView::widget([
+                        'dataProvider' => $dataProvider,
+                        'columns' => [
+                            ['class' => 'yii\grid\SerialColumn', 'header' => 'No'],
 
 //                                            'id',
 //                                            'external_id',
 //                                            'type',
-                                            'nama_berkas',
-                                            'created_at:datetime',
-            //'updated_at',
+                            'nama_berkas',
+                            'created_at:datetime',
+                            //'updated_at',
 
-                                            ['class' => 'common\widgets\ActionColumn','header'=>'Aksi','buttons' => [
-                                                'view'=>function ($url, $model, $key) {
-                                                    return Html::a('<i class="flaticon2-information"></i> Lihat', ['berkas/view','id'=>$model->id,'unit'=>$_GET['unit']], ['class'=>' btn btn-sm btn-pill btn-elevate btn-elevate-air btn-info']);
-                                                },
-                                                'update'=>function ($url, $model, $key) {
-                                                    return Html::a('<i class="flaticon2-edit"></i> Ubah', ['berkas/update','id'=>$model->id,'unit'=>$_GET['unit']], ['class'=>' btn btn-sm btn-pill btn-elevate btn-elevate-air btn-warning']);
-                                                },
-                                                'delete'=>function ($url, $model, $key) {
-                                                    return Html::a('<i class="flaticon2-delete"></i> Hapus', ['berkas/delete','id'=>$model->id,'unit'=>$_GET['unit']], [
-                                                        'class'=>' btn btn-sm btn-pill btn-elevate btn-elevate-air btn-danger',
-                                                        'data-confirm' => Yii::t('yii', 'Apakah anda yakin untuk menghapus item ini?'),
-                                                        'data-method' => 'post',
-                                                    ]);
-                                                }
+                            [
+                                'class' => 'common\widgets\ActionColumn',
+                                'header' => 'Aksi',
+                                'buttons' => [
+                                    'view' => function ($url, $model, $key) {
+                                        return Html::a('<i class="flaticon2-information"></i> Lihat',
+                                            ['berkas/view', 'id' => $model->id, 'unit' => $_GET['unit']],
+                                            ['class' => ' btn btn-sm btn-pill btn-elevate btn-elevate-air btn-info']);
+                                    },
+                                    'update' => function ($url, $model, $key) {
+                                        return Html::a('<i class="flaticon2-edit"></i> Ubah',
+                                            ['berkas/update', 'id' => $model->id, 'unit' => $_GET['unit']],
+                                            ['class' => ' btn btn-sm btn-pill btn-elevate btn-elevate-air btn-warning']);
+                                    },
+                                    'delete' => function ($url, $model, $key) {
+                                        return Html::a('<i class="flaticon2-delete"></i> Hapus',
+                                            ['berkas/delete', 'id' => $model->id, 'unit' => $_GET['unit']], [
+                                                'class' => ' btn btn-sm btn-pill btn-elevate btn-elevate-air btn-danger',
+                                                'data-confirm' => Yii::t('yii',
+                                                    'Apakah anda yakin untuk menghapus item ini?'),
+                                                'data-method' => 'post',
+                                            ]);
+                                    }
 
 
-                                            ]],
-                                            ],
-                        ]); ?>
+                                ]
+                            ],
+                        ],
+                    ]); ?>
 
 
                 </div>
