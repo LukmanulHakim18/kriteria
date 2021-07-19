19516890379eb2a0973e82cb7c2a8d0f019ddd64
Adryan Eka Vandra <adryanekavandra@gmail.com>
2021-07-19 19:29:40 +0700

    tamabhin kuantitatif

--- monitoring/modules/eksekutif/modules/prodi/controllers/LkController.php
+++ monitoring/modules/eksekutif/modules/prodi/controllers/LkController.php
@@ -6,5 +6,5 @@ namespace monitoring\modules\eksekutif\modules\prodi\controllers;
 class LkController extends \akreditasi\modules\kriteria9\modules\prodi\controllers\LkController
 {
 
-    protected $lkView = '@monitoring/modules/eksekutif/modules/prodi/views/lk/isi_kriteria';
+    protected $lkView = '@monitoring/modules/eksekutif/modules/prodi/views/lk/isi-kriteria';
 }
